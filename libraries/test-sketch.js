f5165e18a0ab2a16a4742bbe512aed91ffb23d54
Justin L <43196301+jl860130@users.noreply.github.com>
2020-01-26 23:54:06 -0800

    Update test-sketch.js

--- libraries/test-sketch.js
+++ libraries/test-sketch.js
@@ -1,60 +1,38 @@
+let rad = 60; // Width of the shape
+let xpos, ypos; // Starting position of shape
+
+let xspeed = 2.8; // Speed of the shape
+let yspeed = 2.2; // Speed of the shape
+
+let xdirection = 1; // Left or Right
+let ydirection = 1; // Top to Bottom
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(720, 400);
+  noStroke();
+  frameRate(30);
+  ellipseMode(RADIUS);
+  // Set the starting position of the shape
+  xpos = width / 2;
+  ypos = height / 2;
 }
 
 function draw() {
-  var x = width - 370
-  var y = height - 450
-  background(0,200,0);
-  //first segment 
-  fill(0);
-  line(x, y, 200,30);
-  //second segment 
-  fill(0);
-  line(200, 30, 300,120);
-  // third segment
-  fill(0);
-  line(300, 120, 350,300);
-  //fourth segment 
-  fill(0);
-  line(350, 300, 350,350);
-  //fifth segment
-  fill(0);
-  line(350, 350, 320,370);
-  //sixth segment
-  fill(0);
-  line(320, 370, 200,380);
-  //seventh segment 
-  fill(0);
-  line(200, 380, 100, 450);
-  //eighth segment 
-  fill(0);
-  line(100, 450, 20, 300);
-  //ninth segment 
-  fill(0);
-  line(20, 300, 80, 270 );
-  //tenth segment 
-  fill(0);
-  line(80, 270, 85, 200);
-  //eleventh segment 
-  fill(0);
-  line(85, 200, 110, 180);
-  //twelveth segment
-  fill(0);
-  line(110, 180, 85, 130);
-  //thirteenth segment, connect back to the starting point 
-  fill(0);
-  line(85, 130, 130, 50);
-  //first location, larger cricle
-  fill (200, 100, 30)
-  ellipse(250, 200, 50, 50)
-  strokeWeight(2)
-  stroke(0)
-  // second location, smaller circle
-  fill (150, 130, 50)
-  ellipse(150, 300, 25, 25)
-  //city name: Shanghai  
-   fill(0)
-  textSize(30)
-  text('Shanghai, China',200,480)
+  background(102);
+
+  // Update the position of the shape
+  xpos = xpos + xspeed * xdirection;
+  ypos = ypos + yspeed * ydirection;
+
+  // Test to see if the shape exceeds the boundaries of the screen
+  // If it does, reverse its direction by multiplying by -1
+  if (xpos > width - rad || xpos < rad) {
+    xdirection *= -1;
+  }
+  if (ypos > height - rad || ypos < rad) {
+    ydirection *= -1;
+  }
+
+  // Draw the shape
+  ellipse(xpos, ypos, rad, rad);
 }
